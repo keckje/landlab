d086ed2e06281adebf950715df66c07e74d5853e
Jeffrey Keck <31672064+keckje@users.noreply.github.com>
2025-04-08 12:04:46 -0700

    newest version of DHG

---
+++ src/landlab/components/distributed_hydrology_generator/__init__.py
@@ -0,0 +1,7 @@
+from .distributed_hydrology_generator import DistributedHydrologyGenerator
+from ..distributed_hydrology_generator.downscale_DTW_to_landlab_grid import downscale_DTW_to_landlab_grid
+from ..distributed_hydrology_generator.downscale_to_landlab_grid import downscale_to_landlab_grid
+
+__all__ = ["DistributedHydrologyGenerator",
+           "downscale_DTW_to_landlab_grid",
+           "downscale_to_landlab_grid"]
